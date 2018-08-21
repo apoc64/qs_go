f93d18dd0d16acfa90649482807cef69243175bc
apoc64 <sschwedt@gmail.com>
2018-08-21 14:17:55 -0600

    set up port and godir

--- server.go
+++ server.go
@@ -3,14 +3,25 @@ package main
 import (
   "fmt"
   "net/http"
-  )
+  "os"
+)
+
+func getPort() string {
+  p := os.Getenv("PORT")
+  if p != "" {
+    return ":" + p
+  }
+  return ":3000"
+}
 
 func main() {
+  port := getPort()
+
   fmt.Println("hello world")
 
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello from go")
   })
 
-  fmt.Println(http.ListenAndServe(":8080", nil))
+  fmt.Println(http.ListenAndServe(port, nil))
 }
