55e0a692e230b5fd5717b21d8af7a848bcd74e52
apoc64 <sschwedt@gmail.com>
2018-08-21 15:34:15 -0600

    stubbing out gorilla mux methods spike

--- server.go
+++ server.go
@@ -4,27 +4,41 @@ import (
   "fmt"
   "net/http"
   "os"
+  "encoding/json"
+  "log" // errors
+  "github.com/gorilla/mux"
 )
 
-func getPort() string {
-  p := os.Getenv("PORT")
-  if p != "" {
-    return ":" + p
-  }
-  return ":3000"
-}
+// func getPort() string {
+//   p := os.Getenv("PORT")
+//   if p != "" {
+//     return ":" + p
+//   }
+//   return ":3000"
+// }
 
 func main() {
-  port := getPort()
+  // Init router
+  r := mux.NewRouter()
 
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Request made to /")
-    fmt.Fprintf(w, "Hello from go")
-  })
+  // Route handlers:
+  r.HandleFunc("/api/v1/foods", getFoods).Methods("GET")
+  r.HandleFunc("/api/v1/foods/{id}", getFood).Methods("GET")
+  r.HandleFunc("/api/v1/foods", createFood).Methods("POST")
+  r.HandleFunc("/api/v1/foods/{id}", updateFood).Methods("PATCH")
+  r.HandleFunc("/api/v1/foods/{id}", deleteFood).Methods("DELETE")
 
-  fmt.Println("Listening on port", port)
-  err := http.ListenAndServe(port, nil)
-  if err != nil {
-    panic(err)
-  }
-}
+  log.Fatal(http.ListenAndServe(":3000", r)
+  // port := getPort()
+  //
+  // http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+  //   fmt.Println("Request made to /")
+  //   fmt.Fprintf(w, "Hello from go")
+  // })
+  //
+  // fmt.Println("Listening on port", port)
+  // err := http.ListenAndServe(port, nil)
+  // if err != nil {
+  //   panic(err)
+  // }
+} // end main
