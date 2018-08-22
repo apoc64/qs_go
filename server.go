50589f239b9512fc00a7c4e7118660439611085b
apoc64 <sschwedt@gmail.com>
2018-08-22 12:42:35 -0600

    Sets port to environment variable

--- server.go
+++ server.go
@@ -3,20 +3,20 @@ package main
 import (
   "fmt"
   "net/http"
-  // "os"
+  "os"
   "encoding/json"
   "log" // errors
   "github.com/gorilla/mux"
   "strconv"
 )
 
-// func getPort() string {
-//   p := os.Getenv("PORT")
-//   if p != "" {
-//     return ":" + p
-//   }
-//   return ":3000"
-// }
+func getPort() string {
+  p := os.Getenv("PORT")
+  if p != "" {
+    return ":" + p
+  }
+  return ":3000"
+}
 
 // Food Struct (model)
 type Food struct {
@@ -107,16 +107,16 @@ func main() {
   r.HandleFunc("/api/v1/foods/{id}", updateFood).Methods("PATCH")
   r.HandleFunc("/api/v1/foods/{id}", deleteFood).Methods("DELETE")
 
-  log.Fatal(http.ListenAndServe(":3000", r))
-  // port := getPort()
+  port := getPort()
+  fmt.Println("About to listening on port", port)
+  log.Fatal(http.ListenAndServe(port, r))
   //
   // http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
   //   fmt.Println("Request made to /")
   //   fmt.Fprintf(w, "Hello from go")
   // })
   //
-  fmt.Println("Hello")
-  // fmt.Println("Listening on port", port)
+  // fmt.Println("Hello")
   // err := http.ListenAndServe(port, nil)
   // if err != nil {
   //   panic(err)
