6f18359c4f4dee7c31dcced5609df4dc67ab0da8
apoc64 <sschwedt@gmail.com>
2018-08-26 14:36:58 -0600

    adds food to db, does not pass test due to incorrect response

--- foods_model.go
+++ foods_model.go
@@ -24,9 +24,12 @@ func addFoodToDB(food Food) {
   fmt.Println(food.Name)
   fmt.Println(food.Calories)
   db := db()
-  queryString := fmt.Sprintf("INSERT INTO foods (name, calories) VALUES (%s, %v);", food.Name, food.Calories)
+  fmt.Println(db)
+  queryString := "INSERT INTO foods (name, calories) VALUES ($1, $2)"
   fmt.Println(queryString)
-  if _, err := db.Exec(queryString); err != nil {
+  result, err := db.Exec(queryString, food.Name, food.Calories)
+  if err != nil {
     log.Fatal(err)
   }
+  fmt.Println(result)
 }
