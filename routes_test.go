8c5c9347807741d77da0c186602f4b404737eb7e
apoc64 <sschwedt@gmail.com>
2018-08-26 15:05:36 -0600

    truncates database and restarts identity for each test

--- routes_test.go
+++ routes_test.go
@@ -8,7 +8,24 @@ import (
   "github.com/gorilla/mux"
   "strings"
   "bytes"
+  "log"
 )
+
+func setup() *mux.Router {
+  db := db()
+  if _, err := db.Exec("TRUNCATE TABLE meal_foods RESTART IDENTITY"); err != nil {
+    log.Fatal(err)
+  }
+  if _, err := db.Exec("TRUNCATE TABLE foods RESTART IDENTITY"); err != nil {
+    log.Fatal(err)
+  }
+  if _, err := db.Exec("TRUNCATE TABLE meals RESTART IDENTITY"); err != nil {
+    log.Fatal(err)
+  }
+  r := mux.NewRouter()
+  setRoutes(r)
+  return r
+}
 
 func TestGetPort(t *testing.T) {
   port := getPort()
@@ -18,8 +35,7 @@ func TestGetPort(t *testing.T) {
 }
 
 func TestGetFoods(t *testing.T) { // change for db
-  r := mux.NewRouter()
-  setRoutes(r)
+  r := setup()
   req, _ := http.NewRequest("GET", "/api/v1/foods/", nil)
   rr := httptest.NewRecorder()
   r.ServeHTTP(rr, req)
@@ -32,8 +48,7 @@ func TestGetFoods(t *testing.T) { // change for db
 }
 
 func TestAddFood(t *testing.T) {
-  r := mux.NewRouter()
-  setRoutes(r)
+  r := setup()
   payload := []byte(`{"name":"sushi","calories":300}`)
   req, _ := http.NewRequest("POST", "/api/v1/foods/", bytes.NewBuffer(payload))
   rr := httptest.NewRecorder()
